017b373b41f693db2a3033e60ac436cca214e7bb
Sirvau <sirols50984@stud.noroff.no>
2024-02-28 21:51:16 +0100

    fixed auth error

--- js/routes/home.mjs
+++ js/routes/home.mjs
@@ -3,8 +3,8 @@
 import { displayFeedPosts } from "../content/display-posts.mjs";
 import { displayTopButton } from "../ui/buttons/create.mjs";
 import { fetchPostDetail } from "../content/display-specific-post.mjs";
-/* import { setLoginAuthListener } from "../ui/listeners/auth.mjs";
-import { setRegisterAuthListener } from "../ui/listeners/auth.mjs"; */
+import {setAuthListener} from "../ui/listeners/auth.mjs";
+
 
 
 
@@ -21,8 +21,7 @@ export async function homepage() {
     fetchPostDetail()
 
     displayTopButton();
-/*     setAuthListener();  */
-/*     setLoginAuthListener()
-    setRegisterAuthListener() */
+    setAuthListener();  
+
 
 }
--- js/ui/events/onAuth.mjs
+++ js/ui/events/onAuth.mjs
@@ -1,34 +1,21 @@
-   import { registerUser } from "../../api/auth/register.mjs";
-   import { loginUser } from "../../api/auth/login.mjs";
+import { registerUser } from "../../api/auth/register.mjs";
+import { loginUser } from "../../api/auth/login.mjs";
 
-   
 
-/*    //On Auth Function
-   export async function onAuth(event) {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
 
-    if (location.pathname.includes("/register")) {
-        await registerUser(name, email, password);
-    }
 
-    const login = await loginUser(email,password);
-    console.log(login);
-} 
- */
+
 // On Auth Function
-/* export async function onAuth(event, actionType) {
-    event.preventDefault();
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+export async function onAuth(event, actionType) {
+ event.preventDefault();
+ const name = event.target.name.value;
+ const email = event.target.email.value;
+ const password = event.target.password.value;
 
-    if (actionType === "register") {
-        await registerUser(name, email, password);
-    } else {
-        const login = await loginUser(email, password);
-        console.log(login);
-    }
-} */
+ if (actionType === "register") {
+     await registerUser(name, email, password);
+ } else {
+     const login = await loginUser(email, password);
+     console.log(login);
+ }
+} 
