f3aa6d96ad8a194984a4763372868ff5ccb45221
Sirvau <sirols50984@stud.noroff.no>
2024-03-01 12:29:45 +0100

    remove post function
    Function working with js.
    Not tried in a form yet.

--- js/api/posts/remove.mjs
+++ js/api/posts/remove.mjs
@@ -1,3 +1,26 @@
 
 
-export async function removePost(id) {}
+import {apiV2_BaseUrl ,apiV2_Posts} from "../constants.mjs"
+import { authFetch } from "../fetch.mjs";
+
+
+
+
+const action = `${apiV2_Posts}`;
+const method = "DELETE";
+
+
+export async function removePost(id) {
+    if (!id) {
+        throw new Error("Delete requires a postID");
+    }
+    
+const removePostUrl = `${apiV2_BaseUrl}${action}/${id}`;
+
+const response = await authFetch(removePostUrl, {
+    method
+})
+
+return await response.json();
+
+}
--- js/index.mjs
+++ js/index.mjs
@@ -2,9 +2,11 @@ import { homepage } from "./routes/home.mjs";
 /*  import { updatePost } from "./api/posts/update.mjs";  */
 
 /* import { createPost } from "./api/posts/create.mjs"; */
+/* import { removePost } from "./api/posts/remove.mjs"; */
 
 
 homepage();
+
 
 
 /*  updatePost({
@@ -13,3 +15,7 @@ homepage();
     body: "UPDATED post",
     tags: ["tiger"]
 })   */
+
+
+/* 
+  removePost(639);    */
