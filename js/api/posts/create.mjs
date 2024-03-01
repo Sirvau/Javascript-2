755c9c92b0a0f459985662b72f2b24b3a38e3804
Sirvau <sirols50984@stud.noroff.no>
2024-03-01 11:47:53 +0100

    create post function
    Not using form to create it yet, but tried wth js. It worked.

--- js/api/posts/create.mjs
+++ js/api/posts/create.mjs
@@ -1,1 +1,22 @@
-export async function createPost(postData) {}
+
+import {apiV2_BaseUrl ,apiV2_Posts} from "../constants.mjs"
+import { authFetch } from "../fetch.mjs";
+
+const action = `${apiV2_Posts}`;
+
+
+
+
+export async function createPost(postData) {
+const createPostUrl = `${apiV2_BaseUrl}`+ action;
+
+const response = await authFetch(createPostUrl, {
+    method: "POST",
+    body: JSON.stringify(postData)
+});
+
+const post = await response.json();
+
+console.log(post)
+
+}
--- js/index.mjs
+++ js/index.mjs
@@ -1,5 +1,10 @@
 import { homepage } from "./routes/home.mjs";
+import { createPost } from "./api/posts/create.mjs";
 
 homepage();
 
 
+createPost({
+    title: "Hello hello",
+    body: "My first post"
+})
