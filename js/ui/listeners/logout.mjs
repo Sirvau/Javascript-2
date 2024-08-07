c024e74d89cd848086419b5f51927c8c1bbc12fa
Sirvau <sirols50984@stud.noroff.no>
2024-08-07 18:46:43 +0200

    Added js docs to login, logout, register, search, logout-listener and search-listener functions

--- js/ui/listeners/logout.mjs
+++ js/ui/listeners/logout.mjs
@@ -1,5 +1,14 @@
 import { logout } from "../../api/auth/logout.mjs";
 
+
+/**
+ * Adds an event listener to a DOM element that logs out the user when the element is clicked.
+ *
+ * This function attaches a click event listener to the specified DOM element. When the element
+ * is clicked, the user is logged out by calling the `logout` function, which removes the user's
+ * authentication token and profile from local storage.
+ */
+
 export function logoutListener() {
     const logoutButton = document.getElementById('logout-button');
 
