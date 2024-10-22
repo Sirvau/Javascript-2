dbb312b01c8ac0ce0918db56d481fcdcc28fc8b9
Sirvau <sirols50984@stud.noroff.no>
2024-10-22 20:09:27 +0200

    Added author as quaryparameter in getPosts. Styling adjustments on post feed. Filtered out posts without images

--- js/templates/post.mjs
+++ js/templates/post.mjs
@@ -1,74 +1,86 @@
 
 import * as postMethods from "../api/posts/index.mjs";
- import {apiV2_BaseUrl, apiV2_Posts } from "../api/constants.mjs"; 
+import {apiV2_BaseUrl, apiV2_Posts } from "../api/constants.mjs"; 
 import { authFetch } from "../api/fetch.mjs";
 
 
 
-
-const defaultProfileImageUrl = 'https://images.unsplash.com/photo-1516724562728-afc824a36e84?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
 
 export function postTemplate(postData) {
     
     const title = postData.title;
     const bodyText = postData.body;
-    const postImageUrl = postData.media && postData.media.url ?postData.media.url : defaultProfileImageUrl;
+    const postImageUrl = postData.media && postData.media.url;
     const imgAltText = postData.media?.alt || "";
     const tag = postData.tags;
     const id = postData.id;
     
-
      const post = document.createElement("a");
-     post.classList.add("container", "row", "border", "rounded", "my-5");
+     post.classList.add("container-sm", "d-flex", "flex-column","flex-sm-row","justify-content-center", "w-100" , "mt-3","border-bottom","rounded-1");
      post.href = "#";
      post.addEventListener("click", () => {
-        window.location.href = `/post/?id=${id}`;
+        window.location.href = `/post/?id=${id}`;      
     });
    
-
+ 
      const postImageAndTagContainer = document.createElement("div");
-     postImageAndTagContainer.classList.add("col-sm-4", "my-2");
+    postImageAndTagContainer.style.height = "280px"
+     postImageAndTagContainer.classList.add("my-2", "mw-100");
      const postImage = document.createElement("img");
-     postImage.classList.add("img-thumbnail");
+     postImage.classList.add("img-fluid", "w-100", "h-100", "object-fit-cover", "rounded-1" );
      postImage.src = postImageUrl;
      postImage.alt = imgAltText;
      postImageAndTagContainer.appendChild(postImage);
-     const tags = document.createElement("p");
-     tags.textContent = `${tag}`;
+ 
 
     const postTitleAndBodyContainer = document.createElement("div");
-    postTitleAndBodyContainer.classList.add("col-sm-7", "col-md-8", "my-2");
+    postTitleAndBodyContainer.classList.add("col-sm-6", "col-md-8","my-2", "ms-3");
     const postTitle = document.createElement("h2");
     postTitle.textContent = `${title}`;
-    postTitle.classList.add("h4", "primary-font", "text-light", "text-center");
+    postTitle.classList.add("h4", "primary-font", "text-light");
     postTitleAndBodyContainer.appendChild(postTitle);
     const postBodyText = document.createElement("p");
     postBodyText.textContent = `${bodyText}`;
-    postBodyText.classList.add("secondary-font", "text-light", "text-center");
+    postBodyText.classList.add("secondary-font", "text-light", "align-top");
+
+
+    const tagsContainer = document.createElement("div");
+    tagsContainer.classList.add("tags-container");
+
+
+    tag.forEach((singleTag) => {
+    const tagBadge = document.createElement("span");
+    tagBadge.textContent = singleTag;
+    tagBadge.classList.add("text-light", "badge", "border", "border-rounded", "text-bg-primary", "me-1", "fw-semibold", "font-monospace");
+    tagsContainer.appendChild(tagBadge);
+    });
+
     postTitleAndBodyContainer.appendChild(postBodyText);
+    postTitleAndBodyContainer.appendChild(tagsContainer);
 
-
-post.append(postImageAndTagContainer,postTitleAndBodyContainer )
-
-    
- return post;
+    post.append(postImageAndTagContainer,postTitleAndBodyContainer )
+   
+return post;
  
-}
+};
 
 export async function renderPostTemplate(postData, parent) {
-    postData.forEach((post) => {
+    const filteredPosts = postData.filter(post => post.media && post.media.url);
+    filteredPosts.forEach((post) => {
         const postElement = postTemplate(post);
         parent.appendChild(postElement);
     });
-}
+};
 
 
 export async function displayAllPosts() {
     const posts = await postMethods.getPosts();
     const container = document.querySelector("#nav-feed");
-    await renderPostTemplate(posts.data, container);
-}
+    if (posts && posts.data) {
+        await renderPostTemplate(posts.data, container);
+    }
+};
 
 
 //---------------------------------------------------------------------------------
@@ -133,7 +145,6 @@ export async function fetchPostDetail() {
         linkToRemovePost.addEventListener("click", () => {
             window.location.href = "#";
         })
-
 
         return singlePostContainer;
     } catch (error) {
